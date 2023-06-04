18451725cb8956d5d962e8ab4214e01910e3fd7a
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2023-06-04 13:29:03 +0530

    added msg pop up

--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -6,6 +6,8 @@ const AddProduct = () => {
 
   const user = JSON.parse(localStorage.getItem('user')) || {};
 
+  const [addedStatus, setStatus] = useState(false);
+
   const [formData, setData] = useState({
     name: '',
     qty: '',
@@ -20,10 +22,18 @@ const AddProduct = () => {
     })
   }
 
+  const openMsg = () => {
+    setStatus(true);
+    setTimeout(() => {
+      setStatus(false);
+    }, 4000);
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { data } = await backendInstance.post('/products', formData);
     console.log(data);
+    openMsg();
     e.target.reset();
   }
 
@@ -33,58 +43,74 @@ const AddProduct = () => {
   }
 
   return (
-    <form
-      style={{
-        padding: '16px'
-      }}
-      onSubmit={handleSubmit}
-    >
-      <label htmlFor='name'  >Name</label>
-      <br />
-      <input
-        type='text'
-        id="name"
-        name="name"
-        onChange={handleChange}
-        required
-      />
-      <br />
-      <br />
-      <label htmlFor='qty'  >Quantity</label>
-      <br />
-      <input
-        type='number'
-        id="qty"
-        name="qty"
-        onChange={handleChange}
-        required
-      />
-      <br />
-      <br />
-      <label htmlFor='imageUrl'  >Product Image</label>
-      <br />
-      <input
-        type='url'
-        id="imageUrl"
-        name="imageUrl"
-        onChange={handleChange}
-        required
-      />
-      <br />
-      <br />
-      <label htmlFor='price'  >Price</label>
-      <br />
-      <input
-        type='number'
-        id="price"
-        name="price"
-        onChange={handleChange}
-        required
-      />
-      <br />
-      <br />
-      <input type='submit' />
-    </form>
+    <>
+      <form
+        style={{
+          padding: '16px'
+        }}
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor='name'  >Name</label>
+        <br />
+        <input
+          type='text'
+          id="name"
+          name="name"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <br />
+        <label htmlFor='qty'  >Quantity</label>
+        <br />
+        <input
+          type='number'
+          id="qty"
+          name="qty"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <br />
+        <label htmlFor='imageUrl'  >Product Image</label>
+        <br />
+        <input
+          type='url'
+          id="imageUrl"
+          name="imageUrl"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <br />
+        <label htmlFor='price'  >Price</label>
+        <br />
+        <input
+          type='number'
+          id="price"
+          name="price"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <br />
+        <input type='submit' />
+      </form>
+      {addedStatus && <div
+        style={{
+          height: '50px',
+          width: '200px',
+          backgroundColor: 'green',
+          color: 'white',
+          position: 'absolute',
+          textAlign: 'center',
+          bottom: '50px',
+          right: '20px'
+        }}
+      >
+        <h4>Added Successfully</h4>
+      </div>}
+    </>
   )
 }
 
